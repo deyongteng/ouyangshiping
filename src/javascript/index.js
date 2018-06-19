8250a289b4149d28723f011d22a9bb99f977ee8d
jackteng <tengdeyong1994@gmail.com>
2018-06-19 22:20:06 +0800

    新增点击进入内容详情

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,10 +1,14 @@
 var index = {
-
+    element: null,
+    _data: null,
     init: function ( data ) {
-        this.setHtml( data );
+        this.element = $("#content");
+        this._data = data;
+        this.setHtml();
+        this.binEvent();
     },
     setHtml: function () {
-        var itmeArr = data;
+        var itmeArr = this._data;
         var html = "",time,content;
 
         for( var i=0,l=itmeArr.length; i<l; i++ ){
@@ -20,14 +24,14 @@ var index = {
             }else if( i == itmeArr.length-1 ){
                 html += ' <li class="last">';
             }else{
-                html += ' <li class=>';
+                html += ' <li class="">';
             }
 
-            html += '     <div class="left_liner">';
+            html += '     <div class="left_liner animated slideInLeft">';
             html += '           <span></span>';
             html += '           <span></span>';
             html += '     </div>';
-            html += '       <div class="right_content">';
+            html += '       <div class="right_content animated flipInX">';
             html += '           <div class="content">';
             html += '               <time>'+ time +'</time>';
             html += '               <p>'+ content +'</p>';
@@ -37,5 +41,30 @@ var index = {
         }
 
         $("[name='itme_list']").html( html );
+    },
+
+    binEvent: function () {
+        var _this = this;
+
+        this.element.find("[name='itme_list']").off("click","li").on("click","li",function () {
+
+            _this.setContent( _this._data[$(this).index()] );
+        });
+
+        this.element.find(".return").off("click").on("click",function () {
+            $(".content2").removeClass("active");
+        });
+    },
+
+    setContent: function ( data ) {
+
+        var data = data || '';
+        var $content2 = $(".content2");
+        var text = "&nbsp;&nbsp;&nbsp;&nbsp;"+data.itme.content+"</br>我们的故事还在继续～";
+
+        $content2.addClass("active");
+        $content2.find(".time").text( data.itme.time );
+        $content2.find(".text_content p").html( text )
+
     }
 }
